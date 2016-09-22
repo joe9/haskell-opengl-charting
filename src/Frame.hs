44fe55775d1d0b5ba8764ac9807e6121db0a8234
joe9 <joe9mail@gmail.com>
2016-09-21 22:27:46 -0700

    further changes and modularization

---
+++ src/Frame.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE PackageImports #-}
+
+module Frame where
+
+import Data.Colour.Names
+import qualified Data.Vector.Storable as VS
+import qualified Data.Vector.Unboxed as VU
+import "gl" Graphics.GL
+--
+import GLFWHelpers
+import OpenGLHelpers
+import Scale
+import Types
+import Drawable
+
+drawFrameFunction
+    :: State
+    -> VU.Vector PriceData
+    -> Scale
+    -> Scale
+    -> Scale
+    -> Drawable
+    -> IO (IO ())
+drawFrameFunction _ _ _ _ _ d = do
+    let vertices =
+            VS.fromList [-0.99, -0.99, -0.99, 0.99, 0.99, 0.99, 0.99, -0.99]
+    loadBuffer (dBufferId d) vertices
+    putStrLn ("frame vertices are: " ++ show vertices)
+    return
+        (glDrawArrays
+             GL_LINE_LOOP
+             0
+             (div (fromIntegral (VS.length vertices)) 2))
+
+-- Add a frame for the chart.
+frameDrawable
+    :: VertexArrayId -> BufferId -> Drawable
+frameDrawable vaId bId =
+    Drawable
+    { dDraw = return ()
+    , dLoadBufferAndBuildDrawFunction = drawFrameFunction
+    , dPreviousValue = Nothing
+    , dCurrentValue = (\s _ ->
+                            ValueCursorPosition
+                                (stateCursorX s)
+                                (stateCursorY s))
+    , dVertexArrayId = vaId
+    , dBufferId = bId
+    , dColour = green
+    , dTransparency = Nothing
+    , dType = Frame
+    }
--- src/Screen.hs
+++ src/Screen.hs
@@ -1,35 +1,15 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PackageImports #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeFamilies #-}
 
 module Screen where
 
 --   https://ghc.haskell.org/trac/ghc/wiki/Commentary/Packages/PackageImportsProposal
-import Control.Concurrent
-import Control.Concurrent.Async
 import Data.Bits
 import Data.Colour.Names
-import Data.IORef
-import Data.Time.Clock.POSIX
-import qualified Data.Vector.Storable as VS
-import qualified Data.Vector.Unboxed as VU
 import "gl" Graphics.GL
-import Graphics.UI.GLFW as GLFW
 import Prelude hiding (init)
-import System.Random
 --
+import OpenGLHelpers
 import Drawable
-import PriceGraphOpenGL
-import VolumeGraphOpenGL
-import GLFWStuff
-import MyDataUnboxedVector
-import OpenGLStuff
-import ScaleDataUnboxedVector
-import TypesOpenGL
 
 screenDrawable :: VertexArrayId -> BufferId -> Drawable
 screenDrawable vaId bId =
